aa1637ac79b51d2ae7ac1edb026287dfe02a1ae8
hidec-7 <hiedematoaho0730127@gmail.com>
2021-06-02 22:21:00 +0900

    Itemの紐付け

--- CounterAppWithRxSwift/ViewController.swift
+++ CounterAppWithRxSwift/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .brown
     }
 
+    @IBAction func countUpButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func countDownButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func countResetButton(_ sender: UIButton) {
+        
+    }
 }
 
